fe5ab1887d7d130c0a2bb6ed094f9700f1f7c8f8
EstebanM159 <estebanm91231@gmail.com>
2024-06-24 21:52:59 -0300

    Agregue validacion en el enrutador

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,10 +7,24 @@ import { authenticate } from '../middlewares/auth'
 const router = Router()
 
 router.post('/create-account-withF',AuthController.CreateAccountWithFacebook)
-router.post('/create-account',AuthController.createAccount)
+router.post('/create-account',
+    body('userName').notEmpty().withMessage('El nombre es obligatorio'),
+    body('password').isLength({min:8}).withMessage('La contraseña es muy corta, minimo 8 caracteres'),
+    body('password_confirmation').custom((value,{req})=>{
+        if(value !== req.body.password){
+            throw new Error('Las contraseñas deben ser iguales')
+        }
+        return true
+    }),
+    body('phone').isNumeric().withMessage('El telefono debe ser un numero'),
+    handleInputErrors,
+    body('email').isEmail().withMessage('Email no valido'),
+    AuthController.createAccount
+)
 router.post('/login-withF', AuthController.loginWithFacebook)
 router.post('/login',
     body('email').isEmail().withMessage('Email no valido'),
+    body('password').notEmpty().withMessage('La password no puede ir vacio'),
     handleInputErrors,
     AuthController.login
 )
